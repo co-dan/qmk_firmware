ed617b895d664a80b2ef626c42174938f09be5c1
Dan Frumin <dan@groupoid.moe>
2022-07-17 20:34:38 +0200

    lain config

--- keyboards/dm9records/lain/keymaps/default/keymap.c
+++ keyboards/dm9records/lain/keymaps/default/keymap.c
@@ -16,14 +16,14 @@ enum layers {
 // Tap Dance declarations
 enum {
   TD_CTRL_TAB,
+  TD_SHIFT_CAPS
 };
 
 enum custom_keycodes { LED_EN = SAFE_RANGE, ALT_TAB };
 
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Caps Lock
-  [TD_CTRL_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_LCTRL, KC_TAB),
+  [TD_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
 };
 
 bool is_alt_tab_active = false;
@@ -33,15 +33,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
         KC_GESC,         KC_Q,  KC_W,  KC_E,  KC_R,    KC_T,    KC_LBRC,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
-        TD(TD_CTRL_TAB), KC_A,  KC_S,  KC_D,  KC_F,    KC_G,    KC_RBRC,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_ENTER,
-        KC_LSFT,         KC_Z,  KC_X,  KC_C,  KC_V,    KC_B,               KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BACKSLASH,
-        KC_LCTL,                   KC_LALT, KC_LGUI,  LOWER, KC_SPC,    KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT
+        MT(MOD_LCTL, KC_TAB), KC_A,  KC_S,  KC_D,  KC_F,    KC_G,    KC_RBRC,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_ENTER,
+        TD(TD_SHIFT_CAPS),         KC_Z,  KC_X,  KC_C,  KC_V,    KC_B,               KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BACKSLASH,
+        KC_TAB,                   KC_LALT, KC_LGUI,  LOWER, KC_RCTRL,  KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT
     ),
     [_LOWER] = LAYOUT(
         KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_DEL,
         ALT_TAB, LED_EN,  _______, _______, _______, _______, _______,   _______, _______, KC_LBRACKET, KC_RBRACKET, KC_QUOTE, _______,
         _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______,
-        _______,                   _______, _______, _______, _______,   _______, _______, _______, KC_PGDN, KC_PGUP, _______
+        _______,                   _______, _______, _______, _______,   KC_ENTER, _______, _______, KC_PGDN, KC_PGUP, _______
     ),
     [_RAISE] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,
@@ -104,10 +104,11 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return computed;
 }
 
-/* bool led_update_user(led_t led_state) { */
-/*     lain_set_led(0, led_state.caps_lock); */
-/*     return false; */
-/* } */
+bool led_update_user(led_t led_state) {
+    lain_set_led(0, led_state.caps_lock);
+    return false;
+}
+
 void matrix_scan_user(void) { // The very important timer.
   if (is_alt_tab_active && IS_LAYER_OFF(_LOWER)) {
     unregister_code(KC_LGUI);
