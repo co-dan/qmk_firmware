f6f0df3735756329a2d4bbf3065df60abe790ab1
Dan Frumin <dan@groupoid.moe>
2023-10-01 19:57:09 +0200

    bump claudia

--- keyboards/claudia/keymaps/mijn/keymap.c
+++ keyboards/claudia/keymaps/mijn/keymap.c
@@ -25,16 +25,16 @@ combo_t key_combos[COMBO_COUNT] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT(
-    KC_Q,   KC_W,       KC_E,       KC_R,       KC_T,    KC_Y,    KC_U,       KC_I,       KC_O,             KC_P,
+    KC_Q,   KC_W,       KC_E,       KC_R,       KC_T,    KC_Y,    KC_U,       KC_I,       KC_O,                         KC_P,
     KC_A,   MT_G(KC_S), MT_S(KC_D), MT_C(KC_F), KC_G,    KC_H,    MT_C(KC_J), MT_S(KC_K), MT_G(KC_L),  LT(_CONTROL, KC_SCLN),
-    KC_Z,   KC_X,       KC_C,       KC_V,       KC_B,    KC_N,    KC_M,       KC_COMM,    KC_DOT,         KC_SLSH,
+    KC_Z,   KC_X,       KC_C,       KC_V,       KC_B,    KC_N,    KC_M,       KC_COMM,    KC_DOT,                    KC_SLSH,
                  MT_G(KC_ESC),  LT(_LOWER,KC_BSPC),  LT(_RAISE,KC_SPACE), MT(KC_RALT,KC_ENTER)
   ),
 
   [_RAISE] = LAYOUT(
-    KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,             KC_0,
-    KC_GRV,  KC_MINS, KC_EQL,  KC_BSLS, KC_QUOT, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,KC_QUOTE,
-    KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_COLN, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_BACKSLASH,
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,       KC_8,    KC_9,     KC_0,
+    KC_GRV,  KC_MINS, KC_EQL,  KC_BSLS, KC_QUOT, KC_LEFT, KC_DOWN,    KC_UP,   KC_RIGHT, KC_QUOTE,
+    KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_COLN, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,  KC_BACKSLASH,
                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ), 
 
